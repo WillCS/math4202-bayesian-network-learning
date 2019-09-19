47cad4fba006601a9b524d0c087c1482bfb5e321
WillCS <wcstibbards@gmail.com>
2019-09-19 16:14:34 +1000

    Fixed up cutting plane implementation.

--- src/original_formulation.py
+++ src/original_formulation.py
@@ -25,90 +25,103 @@ def solve(data: Dataset, parent_set_lim: int):
 
     model = Model('Bayesian Network Learning')
 
-    # Cant have a thing be it's own parent
     # Linear variables because we really only care about the linear relaxation
     I = { (W, u): model.addVar()
             for W in parent_sets
-            for u in variables if (not (u in W))
+            for u in variables
     }
 
-    model.setObjective(quicksum(
-            score[W, u] * I[W, u]
-            for W in parent_sets for u in variables if (W, u) in I
+    model.setObjective(quicksum(score[W, u] * I[W, u]
+            for W in parent_sets 
+            for u in variables
     ))
 
     # Only one parent set
     convexity_constraints = { u:
-        model.addConstr(quicksum(I[W, u] for W in parent_sets if (W, u) in I) == 1)
-        for u in variables
+            model.addConstr(quicksum(I[W, u] for W in parent_sets) == 1)
+            for u in variables
     }
-        
-    # Need one var with no parent for DAG
-    sink_constraint = model.addConstr(quicksum(I[emptyset, u] for u in variables) >= 0.99)
-    # ^ We'll replace this with the delayed cluster constraint generation
-
-    model.optimize()
     
-    result = [(W,u) for W in parent_sets for u in variables if (W, u) in I and I[W, u].x > 0]
-    result.sort(key = lambda x:x[1])
+    result = {}
+    last_obj_value = 0
 
-    print(find_cutting_plane(variables, parent_sets, result))
+    while True:
+        model.optimize()
+
+        if abs(last_obj_value - model.objVal) < 0.000000001:
+            break
+
+        last_obj_value = model.objVal
+
+        if model.status == GRB.Status.INFEASIBLE:
+            exit(0)
+
+        result = { (W, u): I[W, u].x 
+                for W in parent_sets
+                for u in variables
+        }
+
+        new_cluster = find_cluster(variables, parent_sets, result)
+
+        model.addConstr(quicksum(I[W, u] for u in new_cluster for W in parent_sets if intersection_size(W, new_cluster) < 1) >= 1)
+
+        # model.addConstr(quicksum(I[W, u] for u in new_cluster for W in parent_sets if not intersects(W, new_cluster) < 2) >= 2)
+
 
     print_parent_visualisation(result)
 
-def find_cutting_plane(variable_range, parent_sets, solution_set):
+def find_cluster(variable_range, parent_sets, solution_set):
     cutting_plane_model: Model = Model('Cutting Plane')
     parent_set_range = range(len(parent_sets))
+    empty_set = parent_sets[0]
 
-    def vertex_in_cluster(v, cluster) -> int:
-        return 1 if v in cluster else 0
-
-    def intersects(W, cluster) -> int:
-        return 1 if len(v for v in cluster if v in W) == 0 else 0
-
-    def in_sols(W, u) -> int:
-        return 1 if (W, u) in solution_set else 0
-
+    # J(W -> u) variables for sub problem
     J = { (W, u): cutting_plane_model.addVar(vtype = GRB.BINARY)
-        for W in parent_set_range
-        for u in variable_range if (not (u in parent_sets[W]))
+        for W in parent_sets
+        for u in variable_range if (W, u) in solution_set
     }
 
     cutting_plane_model.setObjective(quicksum(
-        in_sols(W, u) * J[W, u]
-        for W in parent_set_range
+        solution_set[W, u] * J[W, u]
+        for W in parent_sets
         for u in variable_range if (W, u) in J
     ), GRB.MAXIMIZE)
 
+    # Objective value must be strictly less than 1
     cutting_plane_model.addConstr(quicksum(
-        in_sols(W, u) * J[W, u]
-        for W in parent_set_range
+        solution_set[W, u] * J[W, u]
+        for W in parent_sets
         for u in variable_range if (W, u) in J
     ) <= 0.9999999999)
     
-    cluster_constraints = { (W, u): cutting_plane_model.addConstr(
-            J[W, u] == 1 - quicksum(J[W_prime, u_prime] 
-            for W_prime in parent_set_range
-            for u_prime in parent_sets[W] if (W_prime, u_prime) in J
-        )) for u in variable_range for W in parent_set_range if (W, u) in J
+
+    # If J[empty_set, u] == 1 then u is in the cluster.
+    # These constraints come from (8) in the paper
+    acyclicity_constraints = { (W, u): cutting_plane_model.addConstr(
+            J[W, u] == J[W, u] * (J[empty_set, u] + quicksum(J[empty_set, u_prime] for u_prime in W)))
+            for W in parent_sets
+            for u in variable_range if (W, u) in J
     }
 
     cutting_plane_model.optimize()
 
-    print(cutting_plane_model.feasibility())
-    exit()
-
     new_cluster = [u for u in variable_range if
-            sum(J[W, u].x for W in parent_set_range if (W, u) in J) > 0]
+            sum(J[W, u].x for W in parent_sets if (W, u) in J) > 0]
 
-    return quicksum(I[W, u] for W in parent_set_range for u in variable_range if (W, u) in I) >= 1
+    return new_cluster
 
-# def score_parents(parent_sets, variable_set, scoring_type='RANDOM'):  
-#     if scoring_type == 'VALUE':
-#         score = {(W, u): u for W in parent_sets for u in variable_set}
-#     else:
-#         score = {(W, u): random.random() for W in parent_sets for u in variable_set}
-#     return score
+def intersects(W, cluster) -> int:
+    return 1 if len([v for v in cluster if v in W]) == 0 else 0
+
+def intersection_size(W, cluster) -> int:
+    return len([v for v in cluster if v in W])
+
+def score_parents(parent_sets, variable_set, scoring_type='RANDOM'):  
+    if scoring_type == 'VALUE':
+        score = {(W, u): u for W in parent_sets for u in variable_set}
+    else:
+        score = {(W, u): random.random() for W in parent_sets for u in variable_set}
+    return score
 
 def print_parent_visualisation(res):
     for parent_child_set in res:
